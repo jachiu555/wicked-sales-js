b17f04ea9f19c5fb1c37a499d6bf4bf4d31cef36
James Chiu <jachiu555@gmail.com>
2020-09-19 05:53:59 +0000

    Did some styling on the product-details.jsx component to more closely match the figma. Used the Bootstrap Card and Spacing styles. Added separate div element so longDescription has a place underneath the columns for image and product information.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -28,12 +28,30 @@ export default class ProductDetails extends React.Component {
     return !this.state.product
       ? <h1>Retrieving info...</h1>
       : <>
-        <div className="card col-md-3 m-1" onClick={this.handleClick}>
+        {/* <div className="d-flex justify-content-center m-1" onClick={this.handleClick}>
           <img src={this.state.product.image} className="card-img-top cardImage mx-auto w-50" alt="Card image cap"></img>
           <div className="card-body">
             <h6 className="card-title">{this.state.product.name}</h6>
             <p className="card-price">${(this.state.product.price / 100).toFixed(2)}</p>
             <p className="card-text text-center">{this.state.product.shortDescription}</p>
+          </div>
+        </div> */}
+
+        <div className="card cardDetails">
+          <div className="row no-gutters">
+            <div className="col-md-3">
+              <img src={this.state.product.image} className="card-img"></img>
+            </div>
+            <div className="col-md-2">
+              <div className="card-body ml-3">
+                <h5 className="card-title text-left">{this.state.product.name}</h5>
+                <p className="card-text text-left">${(this.state.product.price / 100).toFixed(2)}</p>
+                <p className="card-text text-left">{this.state.product.shortDescription}</p>
+              </div>
+            </div>
+          </div>
+          <div className="card-text mx-auto">
+            <p>{this.state.product.longDescription}</p>
           </div>
         </div>
       </>;
