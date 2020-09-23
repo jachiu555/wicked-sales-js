73488b9eecd9b06d81c0c0526d601561160835fc
James Chiu <jachiu555@gmail.com>
2020-09-23 04:57:58 +0000

    Fixed some styling on card components. Used rem instead of % to adjust height and width of images and product cards.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -14,11 +14,11 @@ export default class ProductListItem extends React.Component {
   render() {
     return (
       <div className="card col-md-3 m-1" onClick={this.handleClick}>
-        <img src={this.props.item.image} className="card-img-top cardImage mx-auto w-50" alt="Card image cap"></img>
+        <img src={this.props.item.image} className="card-img-top cardImage" alt="Card image cap"></img>
         <div className="card-body">
           <h6 className="card-title">{this.props.item.name}</h6>
           <p className="card-price">${(this.props.item.price / 100).toFixed(2)}</p>
-          <p className="card-text text-center">{this.props.item.shortDescription}</p>
+          <p className="card-text text-left mb-2">{this.props.item.shortDescription}</p>
         </div>
       </div>
     );
