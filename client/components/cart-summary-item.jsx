3337f11c269cdb942aef9b2277212ae6638acb30
James Chiu <jachiu555@gmail.com>
2020-10-28 06:00:29 +0000

    Working on CartSummary and CartSummaryItem front-end. Fixing issues with spacing and cards so cart products are listed in vertical rows.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 export default class CartSummaryItem extends React.Component {
   render() {
     return (
-      <div className="card col-md-3 m-1" onClick={this.handleClick}>
-        <img src={this.props.item.image} className="card-img-top cardImage" alt="Card image cap"></img>
-        <div className="card-body">
-          <h6 className="card-title">{this.props.item.name}</h6>
-          <p className="card-price">${(this.props.item.price / 100).toFixed(2)}</p>
-          <p className="card-text text-left mb-2">{this.props.item.shortDescription}</p>
+      <div className="row mb-2">
+        <div className="col-md-4">
+          <img src={this.props.item.image} className="card-img-top"></img>
+        </div>
+        <div className="col-md-8 mt-5">
+          <h5 className="card-title">{this.props.item.name}</h5>
+          <p className="card-text">${(this.props.item.price / 100).toFixed(2)}</p>
+          <p className="card-text">{this.props.item.shortDescription}</p>
         </div>
       </div>
     );
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -7,10 +7,14 @@ export default class CartSummary extends React.Component {
     const cartArray = this.props.cartState.map(items => <CartSummaryItem cartState={this.props.cart} setView={this.props.setView} item={items} key={items.cartItemId}/>);
 
     return (
-      <div className="col justify-content-right">
-        <h6 className={'backToCatalogButton'} ><button onClick={e => { this.props.setView('catalog', {}); }}>Back to Catalog</button></h6>
-        <div className="column">
-          {cartArray}
+      <div className="container grid-container">
+        <p onClick={e => { this.props.setView('catalog', {}); }}>Back to Catalog</p>
+        <div className="row">
+          <div className="card-body">
+            <div className="card cartSummaryCard">
+              {cartArray}
+            </div>
+          </div>
         </div>
       </div>
     );
