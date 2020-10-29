95b5844bd8d07139d59e094fd36d2de209ea0b09
James Chiu <jachiu555@gmail.com>
2020-10-29 05:12:53 +0000

    Still working on CartSummary and CartSummaryItem styling.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 export default class CartSummaryItem extends React.Component {
   render() {
     return (
-      <div className="row mb-2">
-        <div className="col-md-4">
-          <img src={this.props.item.image} className="card-img-top"></img>
-        </div>
-        <div className="col-md-8 mt-5">
-          <h5 className="card-title">{this.props.item.name}</h5>
-          <p className="card-text">${(this.props.item.price / 100).toFixed(2)}</p>
-          <p className="card-text">{this.props.item.shortDescription}</p>
+      <div className="card container">
+        <div className="row cardDetails mb-3">
+          <img src={this.props.item.image} className="detailImg"></img>
+          <div className="card-body-details col">
+            <h5 className="card-title text-left">{this.props.item.name}</h5>
+            <p className="card-text text-left">${(this.props.item.price / 100).toFixed(2)}</p>
+            <p className="card-text text-left">{this.props.item.shortDescription}</p>
+          </div>
         </div>
       </div>
     );
