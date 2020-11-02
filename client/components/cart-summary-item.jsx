0e208f0642ff04d675b7179a10907a1b7a919012
James Chiu <jachiu555@gmail.com>
2020-11-02 04:14:50 +0000

    Completed Feature 7. Able to add to cart, view cart summary, and see the total price of all items at the bottom of the cart page.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -4,10 +4,10 @@ export default class CartSummaryItem extends React.Component {
   render() {
     return (
       <div className="row cartItem mb-3">
-        <div className="col-md-4">
+        <div className="col-4">
           <img src={this.props.item.image} className="cartImg m-3 p-2"></img>
         </div>
-        <div className="card-body-details col-md-8 mt-3 pt-5">
+        <div className="col-8 mt-5 cartItemDetails">
           <h5 className="card-title ">{this.props.item.name}</h5>
           <p className="card-text ">${(this.props.item.price / 100).toFixed(2)}</p>
           <p className="card-text ">{this.props.item.shortDescription}</p>
